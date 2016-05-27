f7f0354eed0cce9a6b86434870be961829574f56
Chris Wilcoxson <chris@slushman.com>
2016-05-27 15:22:18 -0500

    Added About page content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,5 @@
 var React = require( 'react' );
+var {Link} = require('react-router');
 
 /**
  * Switched to stateless component
@@ -7,8 +8,9 @@ var React = require( 'react' );
 var About = ( props ) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>This is a simple weather app, built using the React.js library.</p>
+			<h1 className="text-center">About</h1>
+			<p>This is a simple weather data fetching app written by <a href="https://slushman.com">Chris Wilcoxson</a>. I am learning how to work with React by building this app.</p>
+			<p>You can find the code for this app at <a href="https://github.com/slushman/react-weather">Github repo.</a></p>
 		</div>
 	);
 };
